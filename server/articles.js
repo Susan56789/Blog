6f7135721a48a00490bcd0bfad810600a4675e8b
Susan Wairimu <mwihaki2susan@gmail.com>
2021-12-17 09:19:16 +0300

    get individual article

--- server/articles.js
+++ server/articles.js
@@ -3,5 +3,9 @@ const ARTICLES = require("./mock-articles");
 module.exports = async (app)=>{
     app.get("/articles", async(req,res)=>{
         res.send(ARTICLES);
-    })
+    });
+
+    app.get("/articles/key", async(req, res)=>{
+      res.send(ARTICLES.filter(article => article.key === req.params.key)[0])
+    });
 }
