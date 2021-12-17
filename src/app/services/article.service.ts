8e048c26727c2df430ce308cc2ebd9ff6ab3fb8f
Susan Wairimu <mwihaki2susan@gmail.com>
2021-12-17 09:43:41 +0300

    get articles from server to clientside

--- src/app/services/article.service.ts
+++ src/app/services/article.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Article } from '../article';
@@ -8,18 +9,20 @@ import { Articles } from '../mock-articles';
 })
 export class ArticleService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getArticles(): Observable<Article[]>{
-    const article: Article[] = Articles;
-
-    return of(article);
+    //const article: Article[] = Articles;
+    //return of(article);
+    return this.http.get<Article[]>("http://localhost:8000/articles")
   }
 
   getArticle(key: string):Observable<Article>{
-    const article: Article[] = Articles.filter(
-      a => a.key === key
-    )
-    return of(article[0]);
+   // const article: Article[] = Articles.filter(
+    //  a => a.key === key
+   // )
+    //return of(article[0]);
+
+    return this.http.get<Article>("http://localhost:8000/articles/"+ key)
   }
 }
